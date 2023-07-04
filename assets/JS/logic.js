79d10f00b3adfe392c7d75aa97ae16986a541ab0
rdheadyii <rdheadyii@gmail.com>
2023-07-04 11:33:35 -0400

    added pseudo code to JS files

---
+++ assets/JS/logic.js
@@ -0,0 +1,15 @@
+// start quiz function on click of start button
+
+// timer starts and first question/answers displays
+
+// answer event if wrong or right should prompt next question
+// if wrong deduct time from timer 
+
+// logic for determining right or wrong answer
+
+// loop through question function until all questions have been given answers and timer hits 0
+
+// stop to question function and log score = read interval value, request user name, and store in local storage
+
+
+
---
+++ assets/JS/questions.js
@@ -0,0 +1,8 @@
+// need array of objects for questions
+
+// Q: "question"
+// A1:
+// A2:
+// A3:
+// A4:
+// CA:
